a54eeadd399638026665b42bcac2e141fb317eb1
Giovanni Roberto Franzan <roberto.franzan@gmail.com>
2019-05-20 10:41:43 +0000

    index.js updated

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //alert("working correctly!");
-
+var gamePattern = [];
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 
-function nextSequence () {
+function newCombination () {
  var randomNumber =  Math.floor(Math.random() * 4);
 
  var randomColor = buttonColors[randomNumber];
+ 
+ gamePattern.push(randomColor);
+    
+ document.querySelector("#"+randomColor);
+    
     }; 
