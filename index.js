7e4f7a0de987c9a10b9112ed275d9c78565e82f7
Giovanni Roberto Franzan <roberto.franzan@gmail.com>
2019-05-20 10:31:48 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//alert("working correctly!");
+
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence () {
+ var randomNumber =  Math.floor(Math.random() * 4);
+
+ var randomColor = buttonColors[randomNumber];
+    }; 
