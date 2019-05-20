1a00c726e37456fc290111e3051353f328e2d8d4
Giovanni Roberto Franzan <roberto.franzan@gmail.com>
2019-05-20 12:22:18 +0000

    UI and JS update

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-//alert("working correctly!");
+var userPattern = [];
+
 var gamePattern = [];
 
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 function newCombination () {
+ 
  var randomNumber =  Math.floor(Math.random() * 4);
 
  var randomColor = buttonColors[randomNumber];
+ var randomColorIdSelector;
+ var randomColorId = "#" + randomColor; 
  
  gamePattern.push(randomColor);
     
- document.querySelector("#"+randomColor);
+ randomColorIdSelector = document.querySelector("#"+randomColor);
+    
+ $(randomColorId).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     
     }; 
+
+
+for (var i = 0 ; i < document.querySelectorAll(".btn").length; i++) {
+   document.querySelectorAll(".btn")[i].addEventListener('click' , function() {
+
+console.log(this.id); 
+var userChosenColor = this.id;
+       });
+       
