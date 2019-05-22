3ab167c96d904ad5e6b9108959d7655ff4ed58d9
Giovanni Roberto Franzan <roberto.franzan@gmail.com>
2019-05-22 22:58:04 +0000

    added animation when button clicked

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
+
 var userPattern = [];
 
 var gamePattern = [];
 
+var userChosenColor;
+
 var buttonColors = ["red", "blue", "green", "yellow"];
 
-function newCombination () {
+
+$(document).ready(function () {
+    
+//Check which button gets pressed
+for (var i = 0 ; i < document.querySelectorAll(".btn").length; i++) {
+   document.querySelectorAll(".btn")[i].addEventListener('click' , function() {
  
+
+userChosenColor = $(this).attr("id");
+userPattern.push(userChosenColor);
+console.log(userPattern);
+
+clickButton(userChosenColor);
+       });
+       
+    }
+    });
+
+
+
+
+//Function to create a random pattern
+function newCombination () {
+
  var randomNumber =  Math.floor(Math.random() * 4);
 
  var randomColor = buttonColors[randomNumber];
  var randomColorIdSelector;
- var randomColorId = "#" + randomColor; 
+ var randomColorId =  
  
  gamePattern.push(randomColor);
     
  randomColorIdSelector = document.querySelector("#"+randomColor);
     
- $(randomColorId).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+ $("#" + randomColor).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
     
     }; 
+    
+    
+// Animate box around clicked buttons
+function clickButton(currentColor) {
 
-
-for (var i = 0 ; i < document.querySelectorAll(".btn").length; i++) {
-   document.querySelectorAll(".btn")[i].addEventListener('click' , function() {
-
-console.log(this.id); 
-var userChosenColor = this.id;
-       });
-       
+  $("#" + userChosenColor).addClass("clicked");
+    
+  setTimeout(function () {
+    $("#" + userChosenColor).removeClass("clicked");
+  }, 500);
+}
